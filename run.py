b44aa0c2c3c16b4766ff4964b87dfa3103eb8923
Miro Frankovic <miro.frankovic@gmail.com>
2018-10-19 21:42:38 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,11 +1,33 @@
 import os
 
-from flask import Flask
+from flask import Flask, redirect
 
 
 app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
+    
+    
+def get_all_messages():
+    return "<br>".join(messages)
 
 
 
+@app.route('/')
+def index():
+    return "To send messages use /USERNAME/MESSAGE"
+    
+    
+@app.route('/<username>')
+def user(username):
+    return "Welcome, {0} - {1}".format(username, get_all_messages())
+    
+    
+@app.route('/<username>/<message>')  
+def send_message(username, message):
+    add_messages(username, message)
+    return redirect(username)
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)    
