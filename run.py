d3bd7ddc2de53659376c68e9f89fdedde4a8c970
Miro Frankovic <miro.frankovic@gmail.com>
2018-10-30 12:03:31 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import os
 from datetime import datetime
+
 from flask import Flask, redirect, render_template, request
 
 
 app = Flask(__name__)
 messages = []
+data = []
 
 
 def write_to_file(filename, data):
@@ -19,14 +21,24 @@ def add_messages(username, message):
              username.title(),
              message))
     
-    
-   
-    
-    
 def get_all_messages():
     """Get all of the messages and separete them by a `br`"""
+    messages = []
+    with open("data/messages.txt", "r") as chat_messages:
+        messages = [row for row in chat_messages if len(row.strip()) > 0]
     return messages
 
+def add_users(username):
+    """Now we added users to the `users` stored in text file"""
+    write_to_file("data/users.txt", "({0}) - {1}\n".format(
+              username.title()))
+        
+def get_all_users():
+    users= []
+    with open("data/users.txt", "r") as user_messages:
+        users = user_messages.readlines()
+    return users
+        
 
 
 @app.route('/', methods=["GET", "POST"])
